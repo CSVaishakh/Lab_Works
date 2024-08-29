443dad08ead211584e52a7c9d96dd340b61a0b9c
C S VAISHAKH <vaishakhsubramonian@gmail.com>
2024-08-29 11:58:44 +0530

    Update linearsearch.c

--- DSA/linearsearch.c
+++ DSA/linearsearch.c
@@ -1,4 +1,31 @@
 #include<stdio.h>
+
+int time;
+int space;
+space = 2*4;
+
+int linear_search(int n,int arr[],int key){
+    int i;
+    space+=4;
+    for(i=0;i<n;i++){
+    	if (key==arr[i]){
+    	   return i;
+    	   time+=1;
+    	}
+    	time+=2;
+    }
+    return 0;
+}
+void main(){
+    int n,i,key;
+    space +=3*4;
+    printf("Enter the no of elements in the list ");
+    scanf("%d",&n);
+    time=+3;
+    int arr[n];
+    space+=n*4;
+    printf("Enter the elements of the list\n");
+    time++;#include<stdio.h>
 int linearsearch(int arr[] , int n , int key){
     int i;
     for(i=0;i<n;i++){
@@ -29,4 +56,26 @@ int main(){
     else{
         printf("The element %d is not present in the lsit",key);
     }
+}
+
+    for(i=0;i<n;i++){
+    	scanf("%d",&arr[i]);
+    	time+=2;
+    }
+    printf("Enter the search key ");
+    scanf("%d",&key);
+    time+=2;
+    int result = linear_search(n,arr,key);
+    space+=4;
+    if (result!=0){
+    	printf("The element is present in the list at the position %d\n",(result+1));
+    	time+=2;
+    }
+    else{
+    	printf("The element is not present in the list\n");
+    	time+=2;
+    }
+    printf("Space Complexity : %d\n",space);
+    time+=2;
+    printf("Time Complexity : %d\n",time); 
 }
