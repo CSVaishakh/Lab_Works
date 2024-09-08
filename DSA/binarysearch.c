3f936f1784dbd4f57f274bb47118a889235d12f4
vaishakh <vaishakhsubramonian@gmail.com>
2024-09-08 10:02:02 +0530

    commit

--- DSA/binarysearch.c
+++ DSA/binarysearch.c
@@ -1,57 +1,43 @@
 #include <stdio.h>
-int time=0;
-int space=2*4;
-
-int binarysearch(int n, int a[], int key){
-    int left = 0, right = n - 1;
-    space += 2*4;
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        space += 4;
-        if (key > a[mid]) {
-            left = mid + 1;
-            time+=2;
-        }
-        else if (key < a[mid]) {
-            right = mid - 1;
-            time+=2;
-        }
-        else{
-            return mid;
-            time++;
-        }
-    }
-    return -1;
-}
-
 void main() {
-    int n, i, key;
-    space += 3*4;
+    int n, i, key, left, right, mid, time=0, space=0, flag;
+    space += 6*4;
     printf("Enter the no. of elements in the list: ");
     scanf("%d", &n);
     time+=2;
-    int a[n];
+    int arr[n];
     space += n*4;
     printf("Enter the elements of the sorted list:\n");
     time++;
     for (i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+        scanf("%d", &arr[i]);
         time+=2;
     }
     printf("Enter the search key: ");
     scanf("%d", &key);
     time+=2;
-    int result = binarysearch(n, a, key);
+    left = 0;
+    right = n - 1;
     time+=2;
-    if (result != -1) {
-        printf("The element %d is present in the list at the position %d\n", key, result+1);
-        time+=2;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        time++;
+        if(key == arr[mid]){
+            printf("Element foud at position %d",mid+1);
+            time+=2;
+            break;
+        }else if(key > arr[mid]){
+            left = mid + 1;
+            time+=2;
+        }else if(key < arr[mid]){
+            right = mid - 1;
+            time+=2;
+        }
     }
-    else {
-        printf("The element is not present in the list\n");
-        time+=2;
+    if(left>right){
+        printf("Element not found");
     }
-    printf("Space Complexity: %d\n", space);
+    printf("\nSpace Complexity: %d\n", space);
     time+=2;
     printf("Time Complexity: %d\n", time);
 }
