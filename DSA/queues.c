5ee42fbca3444173e8ab53f6f684923c37ddf793
C S VAISHAKH <vaishakhsubramonian@gmail.com>
2024-09-09 14:50:29 +0530

    Update queues.c

--- DSA/queues.c
+++ DSA/queues.c
@@ -2,30 +2,27 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #define Max_Size 5
-int queue[size],front = -1,rear = -1;
+int queue[Max_Size],front = -1,rear = -1;
 // function to check if the queue is full
-void isFull(){
+bool isFull(){
     if(rear == Max_Size-1){
-        printf("Queue is full");
-    }else{
-		printf("Queue is not full");
-	}
-    
+        return true;
+    }
+	return false;
 }
 // function to check if the queue is empty
-void isEmpty(){
+bool isEmpty(){
     if(front == -1 && rear == -1){
-        printf("Queue is empty");
-    }else{
-		printf("Queue is not empty")
-	}
+        return true;
+    }
+    return false;
 }
 // function to add an element to the queue
 void enqueue(int item){
     int val;
-    if(rear == Max_Size-1){
+    if(isFull()){
         printf("Queue is full");
-    }else if(front==-1 && rear==-1){
+    }else if(isEmpty()){
         front = rear = 0;
         queue[rear] = item;
     }else{
@@ -35,7 +32,7 @@ void enqueue(int item){
 }
 // function to dequeue an element from the queue
 void dequeue(){
-    if(rear == -1){
+    if(isEmpty()){
         printf("Queue is empty");
     }else{
         printf("Element removed from the queue is %d",queue[front]);
@@ -48,7 +45,7 @@ void dequeue(){
 }
 // function to print the queue
 void display(){
-    if(rear == -1){
+    if(isEmpty()){
         printf("Queue is empty");
     }else{
         for(int i=front;i<=rear;i++){
@@ -61,13 +58,13 @@ void main(){
 	int ch,item;
 	while(true){
 		printf("\n*****Queues*****\n");
-		printf("1.Insertion\n2.Deletion\n3.Display\n4.Is the queue full\n5.Is the queue empty\n6.Exit\n");
+		printf("1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\n");
 		printf("Enter your choice : ");
 		scanf("%d",&ch);
 		printf("\n"); 
 		switch(ch){
 			case 1:
-				printf("Enter the item to be inserted");
+				printf("Enter the item to be enqueued : ");
 				scanf("%d",&item);
 				enqueue(item);
 				break;
@@ -77,17 +74,11 @@ void main(){
 			case 3:
 				display();
 				break;
-			case 4:
-				isFull();
-				break;
-			case 5:        	
-				isEmpty()
-				break;
 		}
-		if(ch==6){
+		if(ch==4){
 			printf("Exiting\n");
 			break;
-		}else if(ch>6){
+		}else if(ch>4){
 			printf("Invalid choice\n");
 		}
 	}
