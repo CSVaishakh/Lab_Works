ceeba88cd286a9b36cd39ddcb22b71ff0049e6d5
vaishakh <vaishakhsubramonian@gmail.com>
2024-10-20 16:32:38 +0530

    commit and push

--- Java_oops!/lab/Abstract_Class.java
+++ Java_oops!/lab/Abstract_Class.java
@@ -1,43 +1,42 @@
 package com.vaishakh.lab;
 
 abstract class Shape {
-    abstract void printArea(double side);
+    abstract void printArea(double... sides);
 }
 
 class Rectangle extends Shape {
     @Override
-    void printArea(double side) {
-        double area = side * side;
+    void printArea(double... sides) {
+        double area = sides[0] * sides[1];
         System.out.println("Area of Rectangle: " + area);
     }
 }
 
 class Triangle extends Shape {
     @Override
-    void printArea(double side) {
-        double area = (Math.sqrt(3) / 4) * side * side;
+    void printArea(double... sides) {
+        double area = 0.5 * sides[0] * sides[1];
         System.out.println("Area of Triangle: " + area);
     }
 }
 
 class Hexagon extends Shape {
     @Override
-    void printArea(double side) {
-        double area = ((3 * Math.sqrt(3)) / 2) * side * side;
+    void printArea(double... sides ) {
+        double area = ((3 * Math.sqrt(3)) / 2) * sides[0] * sides[0];
         System.out.println("Area of Hexagon: " + area);
     }
 }
 
 class ShapeAreaCalculator {
     public static void main(String[] args) {
-        double side = 5.0; // You can change this value to test different side lengths
 
         Shape rectangle = new Rectangle();
         Shape triangle = new Triangle();
         Shape hexagon = new Hexagon();
 
-        rectangle.printArea(side);
-        triangle.printArea(side);
-        hexagon.printArea(side);
+        rectangle.printArea(10,15);
+        triangle.printArea(10,15);
+        hexagon.printArea(10);
     }
 }
