37a4afc696781204222830c480d62ebc2bd6d9f1
vaishakh <vaishakhsubramonian@gmail.com>
2024-10-22 07:25:31 +0530

    commit

--- Java_oops!/lab/FileReader$Writer.java
+++ Java_oops!/lab/FileReader$Writer.java
@@ -1,15 +1,12 @@
-    import java.io.*;
+import java.io.*;
 import java.util.Scanner;
-
 class FileReader$Writer {
     public static void main(String[] args) {
         String s;
         Scanner sc = new Scanner(System.in);
-
         try {
             FileWriter fw = new FileWriter("test.txt");
             System.out.println("Enter lines of text (type 'stop' to end):");
-
             do {
                 s = sc.nextLine();
                 if (!s.equals("stop")) {
@@ -17,18 +14,14 @@ class FileReader$Writer {
                     fw.write("\n");
                 }
             } while (!s.equals("stop"));
-
             fw.close();
-
             try {
                 FileReader fr = new FileReader("test.txt");
                 BufferedReader br = new BufferedReader(fr);
                 System.out.println("Content in the file is:");
-
                 while ((s = br.readLine()) != null) {
                     System.out.println(s);
                 }
-
                 br.close();
                 fr.close();
             } catch (FileNotFoundException fe) {
