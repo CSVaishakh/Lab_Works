e23fee08b979da8d26ed566e3db458fd0cf4eb79
vaishakh <vaishakhsubramonian@gmail.com>
2024-10-23 19:37:07 +0530

    commit

--- DataStructures/16_InsertionSort.c
+++ DataStructures/16_InsertionSort.c
@@ -1,17 +1,12 @@
-//C Program to implement Insertion Sort
 
 #include <stdio.h>
 int main(){
   int array[10], limit, i, j, temp, flag = 0;
-
   printf("INSERTION SORT\n");
   printf("Enter limit: ");
   scanf("%d", &limit);
-
   printf("Enter %d integers: ", limit);
-
   for(i = 0; i < limit; i++) scanf("%d", &array[i]);
-
   for(i = 1; i <= (limit - 1); i++) {
     temp = array[i];
     for(j = i - 1; j >= 0; j--) {
@@ -23,10 +18,7 @@ int main(){
     }
     if(flag) array[j+1] = temp;
   }
-
   printf("Sorted list: ");
-
   for (i = 0; i <= (limit - 1); i++) printf("%d  ", array[i]);
-
   return 0;
 }
