8b832a8d7b5ac4a514348eba59b2cf6b59d5d4e7
vaishakh <vaishakhsubramonian@gmail.com>
2024-11-03 07:57:16 +0530

    commit and push

--- JavaOOPS!/lab/Inheritance.java
+++ JavaOOPS!/lab/Inheritance.java
@@ -1,12 +1,9 @@
-
-
 class Employee {
     String name;
     int age;
     String phoneNumber;
     String address;
     double salary;
-
     Employee(String name, int age, String phoneNumber, String address, double salary) {
         this.name = name;
         this.age = age;
@@ -25,40 +22,32 @@ class Employee {
         printSalary();
     }
 }
-
 class Officer extends Employee {
     String specialization;
-
     Officer(String name, int age, String phoneNumber, String address, double salary, String specialization) {
         super(name, age, phoneNumber, address, salary);
         this.specialization = specialization;
     }
-
     void printDetails() {
         super.printDetails();
         System.out.println("Specialization: " + specialization);
     }
 }
-
 class Manager extends Employee {
     String department;
-
     Manager(String name, int age, String phoneNumber, String address, double salary, String department) {
         super(name, age, phoneNumber, address, salary);
         this.department = department;
     }
-
     void printDetails() {
         super.printDetails();
         System.out.println("Department: " + department);
     }
 }
-
 class EmployeeManagementSystem {
     public static void main(String[] args) {
         Officer officer = new Officer("John Doe", 30, "1234567890", "123 Main St, MANCHESTER City", 75000, "Cybersecurity");
         Manager manager = new Manager("Jane Smith", 35, "9876543210", "456 Elm St, MANCHESTER City", 90000, "IT");
-
         System.out.println("Officer Details:");
         officer.printDetails();
         System.out.println("\nManager Details:");
